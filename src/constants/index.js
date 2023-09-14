6142617710903cef708c72f4c1d5eb13f9d557d6
Abolfazl-Mousavi <a.mousavi.doost.04@gmail.com>
2023-09-14 22:07:21 +0330

    initial commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 //these values will change later
-export const currencyExchangeApiKey = "use your own api key here";
+export const currencyExchangeApiKey =
+  "871eaed90amsh3ffdf74ffd98d89p169ee0jsn7af99fe00c1c";
